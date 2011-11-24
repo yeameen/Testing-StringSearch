084f70656e9c4e75691c651e582005498a3828ee
Chowdhury A Yeameen <ayeameen@yahoo.com>
2011-11-24 13:53:59 -0600

    CharIntCoverage complete

--- test/CharIntMapCoverage.java
+++ test/CharIntMapCoverage.java
@@ -13,11 +13,17 @@ import java.io.*;
 public class CharIntMapCoverage implements StringSearchTests {
 
     private CharIntMap charIntMap;
+    private final String STRING_CHAR_INT = "abracadebra";
     private byte[] storage;
     File temp;
 
+
     private void init() {
-        this.charIntMap = (CharIntMap)new BNDM().processString("abracadebra");
+        this.charIntMap = getCharIntMap(STRING_CHAR_INT);
+    }
+
+    private CharIntMap getCharIntMap(String input) {
+        return (CharIntMap)new BNDM().processString(input);
     }
 
     public CharIntMapCoverage() {
@@ -47,35 +53,47 @@ public class CharIntMapCoverage implements StringSearchTests {
         charIntMap.getLowest();
     }
 
-//    public void testClone() throws Exception {
-//        CharIntMap charIntMap = new CharIntMap();
-//        this.charIntMap.equals(charIntMap);
-//        charIntMap.writeExternal(new ObjectOutputStream(System.out));
-////        charIntMap.toString();
-//    }
+    public void testEquality() {
+        CharIntMap emptyCharIntMap = new CharIntMap();
+        String nonEmptyString = "asdf";
+        CharIntMap nonEmptyCharIntMap = getCharIntMap(nonEmptyString);
+        CharIntMap equivalentCharIntMap = getCharIntMap(STRING_CHAR_INT);
+
+        charIntMap.equals(charIntMap);
+        charIntMap.equals(emptyCharIntMap);
+        charIntMap.equals(nonEmptyCharIntMap);
+        charIntMap.equals(nonEmptyString);
+    }
+
+    public void testHashcode() {
+        charIntMap.hashCode();
+    }
+
+
 
     public void testWriteExternal() throws Exception {
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-////        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(1000)
-//        init();
-//        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-//        objectOutputStream.write(0);
-
-        temp = File.createTempFile("temp", "data");
+        // The persistence can be also implemented with filesystem
+//        temp = File.createTempFile("temp", "data");
 //        temp.deleteOnExit();
-        OutputStream outputStream = new FileOutputStream(temp);
-        charIntMap.writeExternal(new ObjectOutputStream(outputStream));
-        outputStream.close();
+//        FileOutputStream fileOutputStream = new FileOutputStream(temp);
+//        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 
-//        storage = outputStream.toByteArray();
-//        System.out.println("Storage value: " + new String(storage));
-//        outputStream.close();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+        charIntMap.writeExternal(objectOutputStream);
+        objectOutputStream.close();
+
+        storage = outputStream.toByteArray();
 
     }
 
     public void testReadExternal() throws IOException {
-        InputStream inputStream = new FileInputStream(temp);
-        charIntMap.readExternal(new ObjectInputStream(inputStream));
-        inputStream.close();
+        // Open inputstream from storage
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(storage);
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        new CharIntMap().readExternal(objectInputStream);
+        objectInputStream.close();
     }
 }
